4b048288b56f838983a60302dff2191218dede99
ts123 <hukurou@gmail.com>
2013-12-06 02:52:44 +0900

    add uploadcommands

--- after_success.sh
+++ after_success.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 echo after success
 find . -name .git -prune -o -type f
+(cd build/Release && zip -r9 HelloWorld.zip HelloWorld.app)
+
+curl -H "Authorization: token ${TOKEN}" \
+     -H "Accept: application/vnd.github.manifold-preview" \
+     -X POST \
+     -d tag_name=${TRAVIS_BUILD_NUMBER} -d target_commitish=${TRAVIS_COMMIT} -d draft="true" \
+     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${TRAVIS_BUILD_NUMBER}"
+
+curl -H "Authorization: token ${TOKEN}" \
+     -H "Accept: application/vnd.github.manifold-preview" \
+     -H "Content-Type: application/zip" \
+     --data-binary @build/Release/HelloWorld.zip \
+     "https://uploads.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${TRAVIS_BUILD_NUMBER}/assets?name=HelloWorld-${TRAVIS_BUILD_NUMBER}.zip"
+
