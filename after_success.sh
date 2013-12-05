54d4b8a15ec001d4c25a02718a653e856c5dd54f
ts123 <hukurou@gmail.com>
2013-12-06 06:47:48 +0900

    fix after_success

--- after_success.sh
+++ after_success.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 echo after success
 find . -name .git -prune -o -type f
-(cd build/Release && zip -r9 HelloWorld.zip HelloWorld.app)
+
+TARGET=HelloWorld
+TAG_NAME=${TRAVIS_BUILD_NUMBER}
+
+(cd build/Release && zip -r9 ${TARGET}.zip ${TARGET}.app)
+
+getreleaseid() {
+    releaseid=$(curl -s "https://api.github.com/repos/$1/releases" | jq '. | map(select(.tag_name == "'$2'")) | .[0].id')
+    if [ "$releaseid" == "null" ]; then
+      exit 1
+    fi
+    echo $releaseid
+}
 
 curl -H "Authorization: token ${TOKEN}" \
      -H "Accept: application/vnd.github.manifold-preview" \
      -X POST \
-     -d tag_name=${TRAVIS_BUILD_NUMBER} -d target_commitish=${TRAVIS_COMMIT} -d draft="true" \
-     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${TRAVIS_BUILD_NUMBER}"
+     -d '{"tag_name":"'${TAG_NAME}'", "target_commitish":"'${TRAVIS_COMMIT}'", "draft":"true"' \
+     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/releases"
+
+RELEASE_ID=$(getreleaseid ${TRAVIS_REPO_SLUG} ${TAG_NAME})
 
 curl -H "Authorization: token ${TOKEN}" \
      -H "Accept: application/vnd.github.manifold-preview" \
      -H "Content-Type: application/zip" \
-     --data-binary @build/Release/HelloWorld.zip \
-     "https://uploads.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${TRAVIS_BUILD_NUMBER}/assets?name=HelloWorld-${TRAVIS_BUILD_NUMBER}.zip"
+     --data-binary @build/Release/${TARGET}.zip \
+     "https://uploads.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${RELEASE_ID}/assets?name=${TARGET}-${TAG_NAME}.zip"
 
